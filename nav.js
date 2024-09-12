9a13539e2a17802bde38172b56ad35c3ad2fe8a3
piduguramya <piduguramya14e@gmail.com>
2024-09-12 10:29:01 +0530

    project1

---
+++ nav.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+function toggleMenu() {
+    const hide = document.querySelector('.hide');
+    hide.classList.toggle('show');
+}
